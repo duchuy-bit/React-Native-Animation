e272edcfd26d6ea17b842f17da291421a7a203e6
HeaD BeaR <55582430+duchuy-bit@users.noreply.github.com>
2023-07-21 17:05:25 +0700

    init Screen Photography

---
+++ RNAnimation/src/screens/PhotoGraphy/data/data.js
@@ -0,0 +1,27 @@
+export const listPhotoGraphy = [
+    {
+        heading: 'Yamaha R6',
+        imageUri: require('../../../assets/images/PhotoGraphy/image22.jpg'),
+        detail: "The Yamaha YZF-R6 is a sport bike,[1] produced by Yamaha as a 600 class from 1999 to 2020. From 2021, production availability is limited to race-only specification in most global markets,[2][3] causing race organisers to realign their engine eligibility criteria to encourage other manufacturers having larger than 600 cc."
+    },
+    {
+        heading: 'Ducati Panigale V4',
+        imageUri: require('../../../assets/images/PhotoGraphy/image33.jpg'),
+        detail: "The Panigale V4 MY23 represents the last step in the characteristic path of Borgo Panigale's sports bikes. As is the case with motorcycles engaged in MotoGP and WorldSBK, the Panigale V4 is updated and improved year after year, and the 2022 version represents the most significant evolution since its launch year."
+    },
+    {
+        heading: 'Yamaha R15V4',
+        imageUri: require('../../../assets/images/PhotoGraphy/image44.jpg'),
+        detail: "Yamaha R15 V4 is powered by 155 cc engine. This R15 V4 engine generates a power of 18.4 PS @ 10000 rpm and a torque of 14.2 Nm @ 7500 rpm. The claimed mileage of R15 V4 is 60.56 kmpl. Yamaha R15 V4 gets Disc brakes in the front and rear."
+    },
+    {
+        heading: 'Honda CBR150R',
+        imageUri: require('../../../assets/images/PhotoGraphy/image55.jpg'),
+        detail: "The CBR150R was introduced by Honda as the successor to the 2-stroke NSR150. It has been sold in Thai market starting from 2002 and have been exported to many Asian countries and South Africa. A.P. Honda produces the new fuel-injected (PGM-FI) version in 2010, which has lower power than the carburetted version."
+    },
+    {
+        heading: 'Yamaha R1M',
+        imageUri: require('../../../assets/images/PhotoGraphy/image11.jpg'),
+        detail: "The YZF-R1M is a track-oriented street weapon that brings some key performance components to the table such as Öhlins dynamic suspension system,  carbon fiber / composite fairings, and a GPS tracking Yamaha Communications Control Unit to allow for logging your track/ride data."
+    },
+]
--- RNAnimation/src/screens/PhotoGraphy/index.js
+++ RNAnimation/src/screens/PhotoGraphy/index.js
@@ -1,26 +1,140 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View, Dimensions, Image, SafeAreaView,Animated } from 'react-native'
+import React, { useRef } from 'react'
 import TouchableScale from 'react-native-touchable-scale'
 import { useNavigation } from '@react-navigation/native'
+import { listPhotoGraphy } from './data/data';
 
+const { width, height } = Dimensions.get('screen');
 
 export default function PhotoGraphyScreen() {
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
+
+  const scrollX = useRef(new Animated.Value(0)).current;
 
   return (
-    <View>
-        <TouchableScale
-            activeScale={0.8}
-            // tension={20}
-            // friction={7}
-            useNativeDriver
-            style={{width: 200, height: 100,margin: 100, backgroundColor:'pink',}}
-            // onPress={()=> navigation.navigate("")}
-        >
+    <SafeAreaView style={{flex: 1, }}>
+      <Animated.FlatList
+        data={listPhotoGraphy}
+        onScroll={ Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX }}}],
+          {useNativeDriver: true},
+        )}  
+        // initialScrollIndex={2}
+        horizontal
+        pagingEnabled
+        renderItem={({item, index})=> {
+          return(
+            <View style={{height: height, width: width}}>
+              <Image
+                source={item.imageUri}
+                style={{height: height, width: width, resizeMode:'cover', position:'absolute',zIndex: -1}}
+              />
 
-        </TouchableScale>
-    </View>
+              <View style={{position: 'absolute', zIndex: 10,  alignSelf:'center', bottom: 150}}>
+                <TouchableScale
+                  activeScale={0.8}
+                  tension={20}
+                  friction={7}
+                  useNativeDriver
+                  style={{width: width*0.8, height: width*0.38,backgroundColor:'white',borderRadius: 5}}
+                >
+                  <View style={{flexDirection:'row', alignItems:'center',padding: 16}}>
+                    {/* Image */}
+                    <Image
+                      source={require("../../assets/images/avatar.jpg")}
+                      style={{height: 60, width: 60, borderRadius: 100, resizeMode:'contain', overflow:'hidden'}}
+                    />
+                    {/* ------------ */}
+                    <View style={{marginLeft: 16}}>
+                      {/* ------------------ */}
+                      <Text style={{textTransform:'uppercase', fontSize: 16, fontFamily:'Montserrat-Bold',color: "#000"}}>
+                        {item.heading}
+                      </Text>
+                      <Text style={{ fontSize: 14, fontFamily:'Montserrat-Regular',color: "grey"}}>
+                        Delivered by BEAR DEV
+                      </Text>
+                    </View>
+                  </View>
+
+                  {/* ------------------ */}
+                  <View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-around'}}>
+                    {/* Follower */}
+                    <View style={{alignItems:'center'}}>
+                      <Text style={{textTransform:'uppercase', fontSize: 16, fontFamily:'Montserrat-Bold',color: "#000"}}>
+                        1,601
+                      </Text>
+                      <Text style={{ fontSize: 14, fontFamily:'Montserrat-Regular',color: "grey"}}>
+                        Follower
+                      </Text>
+                    </View>
+                    {/* Following*/}
+                    <View style={{alignItems:'center'}}>
+                      <Text style={{textTransform:'uppercase', fontSize: 16, fontFamily:'Montserrat-Bold',color: "#000"}}>
+                        2,001
+                      </Text>
+                      <Text style={{ fontSize: 14, fontFamily:'Montserrat-Regular',color: "grey"}}>
+                        Following
+                      </Text>
+                    </View>
+                    {/* Following */}
+                    <View style={{alignItems:'center'}}>
+                      <Text style={{textTransform:'uppercase', fontSize: 16, fontFamily:'Montserrat-Bold',color: "#000"}}>
+                        23
+                      </Text>
+                      <Text style={{ fontSize: 14, fontFamily:'Montserrat-Regular',color: "grey"}}>
+                        Like
+                      </Text>
+                    </View>
+                  </View>
+                </TouchableScale>
+              </View>
+            </View>
+          )
+        }}
+      />
+
+
+      <View style={{marginTop: 30, marginLeft: 20, position:'absolute',zIndex: 10}}>
+        {
+          listPhotoGraphy.map((item,index)=>{
+            const inputRange = [ (index -0.5) * width, index * width, (index +0.5) * width ]
+            const inputRangeOpacity = [ (index -0.5) * width, index * width, (index +0.5) * width ]
+
+            const opacity = scrollX.interpolate({
+              inputRange: inputRangeOpacity, 
+              outputRange: [ 0, 1, 0 ]
+            })
+
+            const translateX = scrollX.interpolate({
+              inputRange, 
+              outputRange: [ 50, 0, 50 ]
+            })
+
+            return(
+              <Animated.View style={{position:"absolute", zIndex: 100, opacity: opacity,
+                transform:[
+                  {translateY: translateX}
+                ]
+              }}>
+                <Text style={{textTransform:'uppercase', fontSize: 18, fontFamily:'Montserrat-Bold',color: "#FFF"}}>
+                  Bear Dev
+                </Text>
+                <Text style={{fontSize: 14, fontFamily:'Montserrat-Regular',color: "#FFF", maxWidth: width*0.9}}>
+                  {item.detail}
+                </Text>
+              </Animated.View>
+            )
+          })
+        }
+        
+          {/* DEV BEAR
+        </Text>
+        <Text style={{fontSize: 14, fontFamily:'Montserrat-Regular',color: "#FFF"}}>
+        
+        </Text> */}
+      </View>
+    </SafeAreaView>
   )
 }
 
